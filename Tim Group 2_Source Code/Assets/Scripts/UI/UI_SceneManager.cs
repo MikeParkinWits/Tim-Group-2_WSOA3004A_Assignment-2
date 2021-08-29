cbf211e55f4888d2f4788019356deeb9a506e2cf
DiyankaGovender <2104082@students.wits.ac.za>
2021-08-29 20:58:00 +0200

    Menu and Control Scheme Scenes Added

---
+++ Tim Group 2_Source Code/Assets/Scripts/UI/UI_SceneManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_SceneManager : MonoBehaviour
+{
+    public GameObject tutorial_Text;
+
+    void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        StartCoroutine(tutorialText());
+    }
+
+    public IEnumerator tutorialText()
+    {
+        yield return new WaitForSeconds(7f);
+        tutorial_Text.SetActive(false);
+    }
+
+    public void controlSchemeSelector()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void controlScheme1()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void controlScheme2()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void controlScheme3()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void controlScheme4()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
