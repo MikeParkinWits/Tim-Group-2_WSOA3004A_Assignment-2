40edc8e720f142d9c0ea7fd56da7e265e22b5b13
KushKushStudent <1882078@students.wits.ac.za>
2021-09-10 15:17:58 +0200

    Despawning Works!!!!!!

--- Tim Group 2_Source Code/Assets/Scripts/LevelGeneration/LevelObjectSpawner.cs
+++ Tim Group 2_Source Code/Assets/Scripts/LevelGeneration/LevelObjectSpawner.cs
@@ -7,25 +7,29 @@ public class LevelObjectSpawner : MonoBehaviour
     public GameObject playerObject;
     public GameObject[] objects;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+
+    }
     void Start()
     {
      
         int rand = Random.Range(0, objects.Length);
       var newTemp= Instantiate(objects[rand], transform.position,Quaternion.Euler(0,0,Random.Range(0,360)));
       newTemp.transform.parent = this.transform;
-        playerObject = GameObject.FindGameObjectWithTag("Player");
+       // playerObject = GameObject.FindGameObjectWithTag("Player");
        
     }
 
     // Update is called once per frame
     void Update()
-    {/*
-        if (Vector3.Distance(transform.position, playerObject.transform.position) > 100)
-        {
+    {
+         if(Vector3.Distance(transform.parent.gameObject.transform.position, playerObject.transform.position) > 100){
            // Destroy(transform.parent.gameObject);
             Destroy(transform.parent.gameObject);
         }
-       */ 
+       
         //use a physics2d check to see if player is inside area, if the player is inside area, check 6  points around and spawn  levels if they do not exist already, use a physics check to see for other level elements
 
 
